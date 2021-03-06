f4e5aecf3ac218efc5246766fc17d2c8a8d14052
Erik Huang <erikhuangdrums@gmail.com>
2021-03-05 19:14:59 -0800

    commit tree f7c7f05c094e3b74c19714f0c8c5f9903a8f20b0

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^a-z0-9' -]+/ig, "");
+  }
+
+  static titleize(str) {
+    const noNos = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = str.split(' ').map((w, i) => {
+      if (i === 0) return this.capitalize(w);
+      if (!noNos.includes(w.toLowerCase())) return this.capitalize(w);
+      return w;
+    });
+    return words.join(' ');
+  }
 }
